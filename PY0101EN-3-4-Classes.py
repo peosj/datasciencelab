866d552acd55562ccb2707e7ec3ae187c460b7b3
peosj <peosjohn@gmail.com>
2025-12-15 21:41:44 +0800

    Update PY0101EN-3-4-Classes.py

--- PY0101EN-3-4-Classes.py
+++ PY0101EN-3-4-Classes.py
@@ -24,3 +24,46 @@ class Circle(object):
 # Create an object RedCircle
 
 RedCircle = Circle(10, 'red')
+
+dir(RedCircle)
+
+# Print the object attribute radius
+
+RedCircle.radius
+
+# Print the object attribute color
+
+RedCircle.color
+
+# Set the object attribute radius
+
+RedCircle.radius = 1
+RedCircle.radius
+
+# Call the method drawCircle
+
+RedCircle.drawCircle()
+
+# Use method to change the object attribute radius
+
+print('Radius of object:',RedCircle.radius)
+RedCircle.add_radius(2)
+print('Radius of object of after applying the method add_radius(2):',RedCircle.radius)
+RedCircle.add_radius(5)
+print('Radius of object of after applying the method add_radius(5):',RedCircle.radius)
+
+# Create a blue circle with a given radius
+
+BlueCircle = Circle(radius=100)
+
+# Print the object attribute radius
+
+BlueCircle.radius
+
+# Print the object attribute color
+
+BlueCircle.color
+
+# Call the method drawCircle
+
+BlueCircle.drawCircle()
